2bfa5f210c2f9fd5eeffdd050ca3ca97088aaa78
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2022-08-14 13:05:36 +0530

    Ready for deployment.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { ApolloServer } from 'apollo-server';
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core"
+
 import { schema } from './schema';
 import { context } from './context';
 
 export const server = new ApolloServer({
   schema,
   context,
+  introspection: true,
+  plugins: [ApolloServerPluginLandingPageLocalDefault()],
 })
 
 const port = 3000;
